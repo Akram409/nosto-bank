5857e8880ef313b687d501dae70d935e85b308f0
Akram Hossain <akram.iiuc.ctg@gmail.com>
2023-03-12 13:11:53 +0600

    Fixed

--- nosto-bank-main/js/deposit.js
+++ nosto-bank-main/js/deposit.js
@@ -1,3 +1,4 @@
+
 // DRY ---> Do Not Repeat Yourself
 document.getElementById("btn-deposit").addEventListener("click", function () {
   /* 
@@ -5,21 +6,25 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
     2. get the value from the element
     3. convert string value to a number
     */
-  const newDepositAmount = getInputFieldValueById("deposit-field");
+  const newDepositAmount = document.getElementById("deposit-field").value;
   /* 
     1. get previous deposit total by id
 
     */
-  const previousDepositTotal = getTextElementValueById("deposit-total");
+  const previousDepositTotal = document.getElementById("deposit-total").innerText;
 
   // calculate new deposit total
-  const newDepositTotal = previousDepositTotal + newDepositAmount;
-  console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
+  const newDepositTotal =  parseFloat(previousDepositTotal) + parseFloat(newDepositAmount);
+
+  // console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
   // set deposit total value
-  setTextElementValueById("deposit-total", newDepositTotal);
+  document.getElementById("deposit-total").innerText = newDepositTotal
 
   // get previous balance by using the function
-  const previousBalanceTotal = getextElementValueById("balance-total");
-  const newBalanceTotal = previousBalanceTotal + newDepositAmount;
-  setTextElementValueById("balance-total", newBalanceTotal);
-);
+  const previousBalanceTotal =  document.getElementById("balance-total").innerText;
+  
+  const newBalanceTotal = parseFloat(previousBalanceTotal) + parseFloat(newDepositAmount);
+
+  document.getElementById("balance-total").innerText = newBalanceTotal
+});
+
